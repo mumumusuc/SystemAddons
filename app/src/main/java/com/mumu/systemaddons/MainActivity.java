538bf41cbed239817636b8ba8cca73acc6769338
limz <mumumusuc@gmail.com>
2017-10-26 17:25:33 +0800

    first commit

---
+++ app/src/main/java/com/mumu/systemaddons/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mumu.systemaddons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        TextView text = (TextView)findViewById(R.id.text);
+        text.setText("pkg : "+getPackageName()+"\n clz : "+getClass().getName());
+    }
+}
