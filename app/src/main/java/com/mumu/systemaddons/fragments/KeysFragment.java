5948f312b39a7f96cbfc5114270df65fb6220f2a
limz <mumumusuc@gmail.com>
2017-10-27 17:27:07 +0800

    rewrite fragment method

--- app/src/main/java/com/mumu/systemaddons/fragments/KeysFragment.java
+++ app/src/main/java/com/mumu/systemaddons/fragments/KeysFragment.java
@@ -4,11 +4,13 @@ import android.app.LauncherActivity;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.mumu.systemaddons.R;
 
@@ -23,12 +25,17 @@ import static android.content.pm.PackageManager.DONT_KILL_APP;
  * Created by leonardo on 17-10-26.
  */
 
-public class KeysFragment extends PreferenceFragment implements PreferenceManager.OnPreferenceTreeClickListener{
-    private String PREF_KEY_HIDE_ICON;
+public class KeysFragment extends PreferenceFragment implements PreferenceManager.OnPreferenceTreeClickListener {
+
+    private static final int ADVANCE_REBOOT_ENABLE = 0x80;
+    private static final int ADVANCE_REBOOT_BOOTLOADER = 0x01;
+    private static final int ADVANCE_REBOOT_RECOVERY = 0x02;
+
     private String PREF_KEY_ENABLE_SERVICE;
     private String PREF_KEY_ENABLE_SCREEN_ON;
     private String PREF_KEY_ENABLE_MEDIA_NOT_PLAYING;
     private String PREF_KEY_ENABLE_DEBUG;
+    private String PREF_KEY_ENABLE_ADVANCE_REBOOT;
 
     private boolean mEnableService, mEnableScreenOn, mEnableMediaNotPlaying, mEnableDebug;
 
@@ -37,35 +44,34 @@ public class KeysFragment extends PreferenceFragment implements PreferenceManage
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.keys_setting);
 
-        PREF_KEY_HIDE_ICON = getString(R.string.pref_hideIcon);
         PREF_KEY_ENABLE_SERVICE = getString(R.string.pref_service);
         PREF_KEY_ENABLE_SCREEN_ON = getString(R.string.pref_enable_screen_on);
         PREF_KEY_ENABLE_MEDIA_NOT_PLAYING = getString(R.string.pref_enable_media_not_playing);
         PREF_KEY_ENABLE_DEBUG = getString(R.string.pref_enable_debug);
+        PREF_KEY_ENABLE_ADVANCE_REBOOT = getString(R.string.pref_enable_advance_reboot);
 
         init();
     }
 
     @Override // Set for a SwitchPreference so value is always a boolean
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (PREF_KEY_HIDE_ICON.equals(preference.getKey())) {
-            boolean checked = ((SwitchPreference) preference).isChecked();
-            PackageManager manager = getContext().getPackageManager();
-            ComponentName componentName = new ComponentName(getContext(), getActivity().getClass());
-            int invisible = checked ? COMPONENT_ENABLED_STATE_DISABLED_USER : COMPONENT_ENABLED_STATE_DEFAULT;
-            manager.setComponentEnabledSetting(componentName, invisible, DONT_KILL_APP);
-            return true;
-        } else if (PREF_KEY_ENABLE_SERVICE.equals(preference.getKey())) {
+        if (PREF_KEY_ENABLE_SERVICE.equals(preference.getKey())) {
             mEnableService = ((SwitchPreference) preference).isChecked();
             findPreference(PREF_KEY_ENABLE_SCREEN_ON).setEnabled(mEnableService);
             findPreference(PREF_KEY_ENABLE_MEDIA_NOT_PLAYING).setEnabled(mEnableService);
             if (mEnableService) {
-                android.provider.Settings.System.putInt(getContext().getContentResolver(),"volume_long_press_enable",mEnableService?1:0);
+                android.provider.Settings.System.putInt(getContext().getContentResolver(), "volume_long_press_enable", mEnableService ? 1 : 0);
             }
         } else if (PREF_KEY_ENABLE_SCREEN_ON.equals(preference.getKey())) {
-            mEnableScreenOn = ((SwitchPreference) preference).isChecked();
+            mEnableScreenOn = ((CheckBoxPreference) preference).isChecked();
         } else if (PREF_KEY_ENABLE_MEDIA_NOT_PLAYING.equals(preference.getKey())) {
-            mEnableMediaNotPlaying = ((SwitchPreference) preference).isChecked();
+            mEnableMediaNotPlaying = ((CheckBoxPreference) preference).isChecked();
+        } else if (PREF_KEY_ENABLE_ADVANCE_REBOOT.equals(preference.getKey())) {
+            boolean checked = ((SwitchPreference) preference).isChecked();
+            Settings.System.putInt(
+                    getContext().getContentResolver(),
+                    "system_addons_keys_advance_reboot",
+                    checked ? (ADVANCE_REBOOT_ENABLE | ADVANCE_REBOOT_BOOTLOADER | ADVANCE_REBOOT_RECOVERY) : 0);
         } else if (PREF_KEY_ENABLE_DEBUG.equals(preference.getKey())) {
             mEnableDebug = ((SwitchPreference) preference).isChecked();
         }
@@ -74,10 +80,10 @@ public class KeysFragment extends PreferenceFragment implements PreferenceManage
 
     private void init() {
         mEnableService = ((SwitchPreference) findPreference(PREF_KEY_ENABLE_SERVICE)).isChecked();
-        SwitchPreference screen = (SwitchPreference) findPreference(PREF_KEY_ENABLE_SCREEN_ON);
+        CheckBoxPreference screen = (CheckBoxPreference) findPreference(PREF_KEY_ENABLE_SCREEN_ON);
         mEnableScreenOn = screen.isChecked();
         screen.setEnabled(mEnableService);
-        SwitchPreference media = (SwitchPreference) findPreference(PREF_KEY_ENABLE_MEDIA_NOT_PLAYING);
+        CheckBoxPreference media = (CheckBoxPreference) findPreference(PREF_KEY_ENABLE_MEDIA_NOT_PLAYING);
         mEnableMediaNotPlaying = media.isChecked();
         media.setEnabled(mEnableService);
     }
